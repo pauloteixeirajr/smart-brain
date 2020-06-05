7e75054644805d084e9180f439ee45b817ecb794
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-06-05 22:01:52 +0100

    adding profile modal

--- src/components/Profile/ProfileIcon.js
+++ src/components/Profile/ProfileIcon.js
@@ -6,7 +6,7 @@ import {
   DropdownItem,
 } from 'reactstrap';
 
-const ProfileIcon = ({ onRouteChange }) => {
+const ProfileIcon = ({ onRouteChange, toggleModal }) => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const toggle = () => setDropdownOpen((prevState) => !prevState);
 
@@ -35,7 +35,7 @@ const ProfileIcon = ({ onRouteChange }) => {
             backgroundColor: 'rgba(255, 255, 255, 0.5)',
           }}
         >
-          <DropdownItem>View Profile</DropdownItem>
+          <DropdownItem onClick={toggleModal}>View Profile</DropdownItem>
           <DropdownItem onClick={() => onRouteChange('signout')}>
             Sign Out
           </DropdownItem>
