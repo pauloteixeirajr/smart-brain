3ba3e1a2b31e320990ca53d2e96027a25c3bc86e
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-06-05 22:51:23 +0100

    update user profile from modal

--- src/App.js
+++ src/App.js
@@ -149,6 +149,7 @@ class App extends Component {
           <Modal>
             <Profile
               user={user}
+              loadUser={this.loadUser}
               isProfileOpen={isProfileOpen}
               toggleModal={this.toggleModal}
             />
